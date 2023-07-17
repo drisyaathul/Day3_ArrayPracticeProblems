784fb87bd8360e76372c7ddcf3595792fc9293a2
drisyaathul <drisyak809@gmail.com>
2023-07-18 00:25:56 +0800

    Added: Elements of an Array

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("*** Array Practice Problems ***");
+
+        //1. Print Elements of an Array
+        String[] array = {"Drisya","Varsha","Deepa"};
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+
     }
 }
