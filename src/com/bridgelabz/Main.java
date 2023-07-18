a880601588eab04a22b7194a18910cdce3b1a746
drisyaathul <drisyak809@gmail.com>
2023-07-18 22:12:58 +0800

    Added: Duplicate Elements of an Array

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -93,5 +93,20 @@ public class Main {
             System.out.println("Index = " +i+"," + "Value =" +array7[i]);
         }
         System.out.println();
+        //8.Print the duplicate elements of an array
+        System.out.println("Duplicate elements of an array :-");
+
+        int[] arr = {15, 25, 35, 60, 40, 35, 40};
+        System.out.println("Array = "+ Arrays.toString(arr));
+
+        System.out.println("Duplicate elements in an array ");
+        for(int i = 0; i < arr.length; i++){
+            for (int j = i + 1; j < arr.length; j++){
+                if  (arr[i] == arr[j]){
+                    System.out.println("Duplicate of Elements in array is  " + arr[j]);
+                }
+            }
+        }
+
     }
 }
